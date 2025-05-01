3016bfcf708e30347a36458a7ae4917bed292ced
ariyan <ariyanbolandi3@gmail.com>
2025-05-01 23:38:46 +0330

    just create the stack.py file

--- py_drills/data_structure/data-structure.py
+++ py_drills/data_structure/data-structure.py
@@ -19,55 +19,55 @@
 # if __name__ == "__main__":
 #     main()
 
-class Stack:
-    def __init__(self):
-        self.items = []
-
-    def __str__(self):
-        return str(self.items)
-
-    def push(self, item):
-        self.items.append(item)
-
-    def pop(self):
-        return self.items.pop()
-
-    def clear(self):
-        return self.items.clear()
-
-    def peek(self):
-        return self.items[-1]
-
-    def is_empty(self):
-        if not self.items:
-            return True
-        else:
-            return False
-
-
-class Queue:
-
-    def __init__(self):
-        self.items = []
-
-    def __str__(self):
-        return str(self.items)
-
-    def enqueue(self, item):
-        self.items.insert(0, item)
-
-    def dequeue(self):
-        return self.items.pop()
-
-
-stack = Stack()
-
-stack.push("apple")
-stack.push("banana")
-stack.pop()
-stack.push("cucumber")
-stack.push("cherry")
-stack.push("watermelon")
-
-print(stack)
+# class Stack:
+#     def __init__(self):
+#         self.items = []
+#
+#     def __str__(self):
+#         return str(self.items)
+#
+#     def push(self, item):
+#         self.items.append(item)
+#
+#     def pop(self):
+#         return self.items.pop()
+#
+#     def clear(self):
+#         return self.items.clear()
+#
+#     def peek(self):
+#         return self.items[-1]
+#
+#     def is_empty(self):
+#         if not self.items:
+#             return True
+#         else:
+#             return False
+#
+#
+# class Queue:
+#
+#     def __init__(self):
+#         self.items = []
+#
+#     def __str__(self):
+#         return str(self.items)
+#
+#     def enqueue(self, item):
+#         self.items.insert(0, item)
+#
+#     def dequeue(self):
+#         return self.items.pop()
+#
+#
+# stack = Stack()
+#
+# stack.push("apple")
+# stack.push("banana")
+# stack.pop()
+# stack.push("cucumber")
+# stack.push("cherry")
+# stack.push("watermelon")
+#
+# print(stack)
 
---
+++ py_drills/data_structure/stack.py
@@ -0,0 +1,6 @@
+class Node:
+    def __init__(self, head, data):
+        self.head = head
+        self.data = data
+    
+
